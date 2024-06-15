48c4609fc042bd94b1f1947b9b0ab91716c5414a
emanuelikreuz <168487645+emanuelikreuz@users.noreply.github.com>
2024-06-15 16:04:11 -0300

    Update InteracaoUsuario.java

---
+++ src/InteracaoUsuario.java
@@ -0,0 +1,15 @@
+public class InteracaoUsuario {
+    //menu que ira interagir com o usuario
+        public void apresentaMenu(){
+            System.out.println("\nMenu:");
+            System.out.println("1. Listar todos os produtos");
+            System.out.println("2. Listar todos os clientes");
+            System.out.println("3. Filtrar produtos por preço");
+            System.out.println("4. Filtrar clientes por idade");
+            System.out.println("5. Adicionar Produto");
+            System.out.println("6. Sair");
+            System.out.print("Escolha uma opção: ");
+        }
+    
+    }
+    
